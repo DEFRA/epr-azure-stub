d93bf7ad5bf390cdfd0dbf3c6b47435254759ae7
Tim Peel <tim.peel@equalexperts.com>
2026-06-11 16:52:00 +0100

    Remove unnecessary code

---
+++ EprAzureStub/Endpoints.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EprAzureStub;
+
+[ExcludeFromCodeCoverage]
+public static class Endpoints
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder app)
+    {
+        // Endpoints will be added here
+    }
+}
